a76fb1406e09c47ac088cec96a695a4d66863f91
RohitVarri <rohit@github.com>
2026-03-16 15:56:22 +0530

    fix deployment

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, UploadFile, File, Request
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from api.analyze import router
+from backend.api.analyze import router
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-
 app.include_router(router)
 
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+templates = Jinja2Templates(directory="backend/templates")
+
+@app.get("/")
+def home():
+    return {"message": "Skill Gap Analyzer running"}
